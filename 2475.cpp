8469552ae2f8f66fa9141c3c74a6541a5210a8a8
Chetan0402 <chetansharma67g@gmail.com>
2024-10-29 17:15:51 +0530

    prac

---
+++ 2475.cpp
@@ -0,0 +1,19 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        int count=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]!=nums[j] && nums[j]!=nums[k] && nums[k]!=nums[i]) count++;
+                }
+            }
+        }
+        return count;
+    }
+};
