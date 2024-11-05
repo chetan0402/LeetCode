6bbcbe4be683aed6a4761a58052f35253f50e76f
Chetan0402 <chetansharma67g@gmail.com>
2024-11-05 07:45:36 +0530

    5 nov

---
+++ 2914.cpp
@@ -0,0 +1,13 @@
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int minChanges(string s) {
+        int ans=0;
+        for(int i=0;i<s.size()-1;i+=2){
+            if(s[i]!=s[i+1]) ans++;
+        }
+        return ans;
+    }
+};
