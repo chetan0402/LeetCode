6eb4391e4fd8f7064cc908414105353f1ce60333
Chetan0402 <chetansharma67g@gmail.com>
2025-10-21 18:18:10 +0530

    21 oct & fix

---
+++ 3346.cpp
@@ -0,0 +1,63 @@
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        unordered_map<int,int> cnt;
+        for(int i=0;i<n;i++) cnt[nums[i]]++;
+
+        int ans=0;
+
+        for(int i=0;i<n;i++){
+            int lower=i;
+            int left=0;
+            int right=i;
+            
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(nums[mid]>=nums[i]-k){
+                    lower=mid;
+                    right=mid-1;
+                }else left=mid+1;
+            }
+
+            int upper=i;
+            left=i;
+            right=n-1;
+
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(nums[mid]<=nums[i]+k){
+                    upper=mid;
+                    left=mid+1;
+                }else right=mid-1;
+            }
+
+            int cur=cnt[nums[i]]+min(numOperations,upper-lower+1-cnt[nums[i]]);
+            ans=max(ans,cur);
+        }
+
+        for(int i=0;i<n;i++){
+            int upper=i;
+            int left=i;
+            int right=n-1;
+            
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(nums[mid]<=nums[i]+2*k){
+                    upper=mid;
+                    left=mid+1;
+                }else right=mid-1;
+            }
+
+            ans=max(ans,min(numOperations,upper-i+1));
+        }
+
+        return ans;
+    }
+};
