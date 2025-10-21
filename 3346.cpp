2c0a2a16a0286a3459ba28c603d3cfa4afe385ab
Chetan0402 <chetansharma67g@gmail.com>
2025-10-21 18:27:07 +0530

    optimized

--- 3346.cpp
+++ 3346.cpp
@@ -61,3 +61,71 @@ public:
         return ans;
     }
 };
+
+class SolutionSameLogicButOptimized {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        unordered_map<int,int> cnt;
+        for(int i=0;i<n;i++) cnt[nums[i]]++;
+
+        int ans=0;
+        int prevLower=0;
+        int prevUpper=0;
+
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            int lower=prevLower;
+            int left=prevLower;
+            int right=i;
+            
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(nums[mid]>=nums[i]-k){
+                    lower=mid;
+                    right=mid-1;
+                }else left=mid+1;
+            }
+
+            int upper=prevUpper;
+            left=prevUpper;
+            right=n-1;
+
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(nums[mid]<=nums[i]+k){
+                    upper=mid;
+                    left=mid+1;
+                }else right=mid-1;
+            }
+
+            prevLower=lower;
+            prevUpper=upper;
+            int cur=cnt[nums[i]]+min(numOperations,upper-lower+1-cnt[nums[i]]);
+            ans=max(ans,cur);
+        }
+
+        prevUpper=0;
+
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            int upper=prevUpper;
+            int left=prevUpper;
+            int right=n-1;
+            
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(nums[mid]<=nums[i]+2*k){
+                    upper=mid;
+                    left=mid+1;
+                }else right=mid-1;
+            }
+
+            prevUpper=upper;
+            ans=max(ans,min(numOperations,upper-i+1));
+        }
+
+        return ans;
+    }
+};
