64ddc4359393835afac8623775e559e3023f882f
Chetan0402 <chetansharma67g@gmail.com>
2026-01-31 15:55:01 +0530

    31 jan

---
+++ 744.cpp
@@ -0,0 +1,10 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(auto c:letters) if(c>target) return c;
+        return letters[0];
+    }
+};
