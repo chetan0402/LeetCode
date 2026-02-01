ed78b2bdb46e2dc85d407014d793209fb13aa211
Chetan0402 <chetansharma67g@gmail.com>
2026-02-01 09:14:45 +0530

    1 feb

---
+++ 3010.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int min1=60;
+        int min2=60;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<min1){
+                min2=min1;
+                min1=nums[i];
+            }else if(nums[i]<min2) min2=nums[i];
+        }
+        return nums[0]+min1+min2;
+    }
+};
