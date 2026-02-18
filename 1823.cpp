5c1d5fdbc113478249f8f47eb4cf774f8ced016b
Chetan0402 <chetansharma67g@gmail.com>
2026-02-18 21:35:04 +0530

    18 feb

---
+++ 1823.cpp
@@ -0,0 +1,21 @@
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> q;
+        for(int i=1;i<=n;i++) q.push(i);
+
+        while(q.size()>1){
+            int _k=k-1;
+            while(_k--){
+                q.push(q.front());
+                q.pop();
+            }
+            q.pop();
+        }
+
+        return q.front();
+    }
+};
