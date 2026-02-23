a3a918dc787b05a6204650c6d401ca66b12d8f57
Chetan0402 <chetansharma67g@gmail.com>
2026-02-23 14:17:59 +0530

    23 feb

---
+++ 754.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target=abs(target);
+        int k=0;
+        while(target>0){
+            k++;
+            target-=k;
+        }
+
+        if(target%2==0) return k;
+        return k+1+k%2;
+    }
+};
