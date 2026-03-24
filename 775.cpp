801cc1a54fc365378b873ef4d76454752f118f6a
Chetan0402 <chetansharma67g@gmail.com>
2026-03-24 15:52:34 +0530

    24 mar

---
+++ 775.cpp
@@ -0,0 +1,11 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isIdealPermutation(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++) if(abs(nums[i]-i)>1) return false;
+        return true;
+    }
+};
