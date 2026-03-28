3c28857b577ed47cd551331869aa2e0576f02f94
Chetan0402 <chetansharma67g@gmail.com>
2026-03-28 09:19:22 +0530

    b2tree

---
+++ 1080.cpp
@@ -0,0 +1,23 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* sufficientSubset(TreeNode* root, int limit) {
+        if(root==nullptr) return nullptr;
+        if(root->left==nullptr & root->right==nullptr){
+            if(root->val<limit) return nullptr;
+            return root;
+        }
+        root->left=sufficientSubset(root->left,limit-root->val);
+        root->right=sufficientSubset(root->right,limit-root->val);
+        if(root->left==nullptr & root->right==nullptr) return nullptr;
+        return root;
+    } 
+};
