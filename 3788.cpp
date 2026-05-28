83b2aae345c7fb48c42c0ae2e957b127e7257376
Chetan0402 <chetansharma67g@gmail.com>
2026-05-28 20:16:52 +0530

    greedy

---
+++ 3788.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    long long maximumScore(vector<int>& nums) {
+        int n=nums.size();
+
+        vector<long long> prefixSum(n);
+        prefixSum[0]=nums[0];
+        for(int i=1;i<n;i++) prefixSum[i]=prefixSum[i-1]+nums[i];
+
+        vector<int> suffixMin(n);
+        suffixMin[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--) suffixMin[i]=min(suffixMin[i+1],nums[i+1]);
+
+        auto ans=prefixSum[0]-suffixMin[0];
+        for(int i=1;i<n-1;i++) ans=max(ans,prefixSum[i]-suffixMin[i]);
+        return ans;
+    }
+};
